fcb1333922df1260c7e18c1ea129852602a6bcef
Muhammad Safiullah Khan <145552372+safi-thecoder@users.noreply.github.com>
2023-11-13 23:47:05 +0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,17 @@
+function showTime() {
+    var date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let session = "AM";
+
+    if(hours>=12? session = "PM" : session);
+    if(minutes<= 9? minutes = "0"+ minutes : minutes);
+    if(seconds<= 9? seconds = "0"+ seconds : seconds);
+    if(hours > 12? hours = hours - 12 : hours);
+    
+    document.getElementById("timeVariable").innerText =
+    hours + " : " + minutes + " : " + seconds + " " + session;
+
+    setTimeout(showTime,1000);
+}
